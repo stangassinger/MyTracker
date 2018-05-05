49c75d7af4fdf2b64f02f9c2f412a53b6dca8995
guenther <guenther.stangassinger@gmx.de>
2018-05-05 18:43:18 +0200

    adding service

--- app/src/main/java/com/example/android/mytracker/MainActivity.java
+++ app/src/main/java/com/example/android/mytracker/MainActivity.java
@@ -15,83 +15,42 @@
  */
 package com.example.android.mytracker;
 
-import android.location.Location;
 import android.app.Activity;
+import android.app.Service;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends Activity implements
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        LocationListener {
 
-    private final String TAG = "MyAwesomeApp";
+public class MainActivity extends Activity {
+
+    private final String TAG = "MyTracker";
 
     private TextView mLocationView;
 
-    private GoogleApiClient mGoogleApiClient;
 
-    private LocationRequest mLocationRequest;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         mLocationView = new TextView(this);
-
         setContentView(mLocationView);
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(LocationServices.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
+        mLocationView.setText("Location received: blubber");
+        Log.i(TAG, "---->  onCreate");
+        startService(new Intent(getBaseContext(), GPSTracker.class));
     }
 
     @Override
-    protected void onStart() {
+    public void onStart(){
         super.onStart();
-        // Connect the client.
-        mGoogleApiClient.connect();
+        Log.i(TAG, "---->  onStart");
     }
 
-    @Override
-    protected void onStop() {
-        // Disconnecting the client invalidates it.
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
 
-    @Override
-    public void onConnected(Bundle bundle) {
 
-        mLocationRequest = LocationRequest.create();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(1000); // Update location every second
-
-        LocationServices.FusedLocationApi.requestLocationUpdates(
-                mGoogleApiClient, mLocationRequest, this);
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.i(TAG, "GoogleApiClient connection has been suspend");
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.i(TAG, "GoogleApiClient connection has failed");
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        mLocationView.setText("Location received: " + location.toString());
-    }
 }
