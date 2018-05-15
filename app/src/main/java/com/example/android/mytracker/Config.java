c01ff07b792e5ad7c87b70366ea5c06297246ad2
Stangassinger <guenther.stangassinger@gmx.de>
2018-05-15 13:43:19 +0000

    minor change

--- app/src/main/java/com/example/android/mytracker/Config.java
+++ app/src/main/java/com/example/android/mytracker/Config.java
@@ -1,7 +1,7 @@
 package com.example.android.mytracker;
 
 public class Config {
-    public static final String EMAIL ="------"; //your-gmail-username
-    public static final String PASSWORD ="----------"; //your-gmail-password
-    public static final String SEND_TO ="----------"; //send to
+    public static final String EMAIL    ="------"; //your-gmail-username
+    public static final String PASSWORD ="------"; //your-gmail-password
+    public static final String SEND_TO  ="-------"; //send to
 }
